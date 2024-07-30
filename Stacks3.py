47ad4856c7467b471787243d864fd709298b618f
helanwang <christylan1121@gmail.com>
2024-07-30 12:53:24 -0700

    1

---
+++ Stacks3.py
@@ -0,0 +1,12 @@
+
+stack = [3, 4, 5]
+stack.append(6)
+stack.append(7)  # list.append() takes exactly one
+
+print(stack)
+
+stack.pop()  # you can only pop starting from the last item. Can't pop anything directly in the middle.
+stack.pop()
+
+print(stack)
+
